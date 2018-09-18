b278adb11838add6d6824a9296eaab939647a753
Maximo DeLaRosa <mjd809@gmail.com>
2018-09-18 12:41:14 -0400

    Added token auth

--- eats/urls.py
+++ eats/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
   path('', views.index),
   path('api/list', views.get_rest_list),
   path('api/view', views.FoodList.as_view()),
-  path(r'^login/$',views.login_view, name="login"),
-  path(r'^signup/$',views.signup_view, name="signup"),
+  # path(r'^login/$',views.login_view, name="login"),
+  # path(r'^signup/$',views.signup_view, name="signup"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
