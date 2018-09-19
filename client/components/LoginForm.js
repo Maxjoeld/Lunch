b527c3a725297d77ae0ced6d3eab27cbb1f8aba6
Maximo DeLaRosa <mjd809@gmail.com>
2018-09-19 11:55:21 -0400

    Added login functionality

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, AppRegistry, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
 
 class LoginForm extends React.Component {
@@ -9,33 +9,31 @@ class LoginForm extends React.Component {
     password: ''
   };
 
-  handle_change = e => {
-    const name = e.target.name;
-    const value = e.target.value;
-    this.setState(prevstate => {
-      const newState = { ...prevstate };
-      newState[name] = value;
-      return newState;
-    });
-  };
-
   render() {
     return (
-      <View onPress={e => this.props.handle_login(e, this.state)}>
-        <Text>Log In</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>Log In</Text>
         <FormLabel htmlFor="username">Username</FormLabel>
         <FormInput
           type="text"
+          name="username"
           value={this.state.username}
-          onChangeText={this.handle_change}
+          onChangeText={(username) => this.setState({username})}
         />
         <FormLabel>Password</FormLabel>
         <FormInput
           type="password"
+          name="password"
           value={this.state.password}
-          onChange={this.handle_change}
+          onChangeText={(password) => this.setState({password})}
         />
-        <Button title="submit" />
+        <View style={{ display: 'flex', width: '100%', alignItems: 'center',justifyContent: 'center'}}>
+          <Text style={styles.button} onPress={e => this.props.handle_login(e, this.state)}>Submit</Text>
+          <Text style={styles.account}>
+            Don't have an Account ? 
+            <Text onPress={() => this.props.navigation.navigate('SignUp')}>Sign Up</Text>
+          </Text>
+        </View>
       </View>
     );
   }
@@ -46,3 +44,33 @@ export default LoginForm;
 LoginForm.propTypes = {
   handle_login: PropTypes.func.isRequired
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',    
+    width: '100%',
+    fontSize: 24,
+    marginBottom: 40,
+
+  },
+  account: {
+    marginTop: 50,
+  },
+  button: {
+    display: 'flex',
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    width: '100%',
+    height: 30,
+    color: 'white',
+    backgroundColor: 'black',
+  }
+});
